56bf6edfb7c1e9dde2f2eae1683a3527e39cfc0b
Elemental Guardian <32121785+ElementalGuardian@users.noreply.github.com>
2020-07-14 00:50:11 -0700

    Add files via upload

---
+++ Percentages.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+A = 0
+B = 0
+C = 0
+
+print("please enter a percentage value (ex. .75) for A")
+A = float(input())
+#float(input()) =
+print("please enter the value A will be multiplied by")
+B = int(input())
+
+print("A * B = ", float(A) * int(B))
