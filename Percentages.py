eb8a229891348976f4336e0637a0daace9a48541
Elemental Guardian <32121785+ElementalGuardian@users.noreply.github.com>
2020-08-07 18:32:20 -0700

    Update Percentages.py

--- Percentages.py
+++ Percentages.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
-A = 0
-B = 0
-C = 0
-
 print("please enter a percentage value (ex. .75) for A")
 A = float(input())
-#float(input()) =
+
 print("please enter the value A will be multiplied by")
 B = int(input())
 
