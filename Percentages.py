47051b78d68370f1f1b0498480bf3320c88f42b4
60MilesPerHour <32121785+60MilesPerHour@users.noreply.github.com>
2020-08-26 20:56:59 -0700

    Update Percentages.py

--- Percentages.py
+++ Percentages.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
-
-print("please enter a percentage value (ex. .75) for A")
-A = float(input())
-
-print("please enter the value A will be multiplied by")
-B = int(input())
-
-print("A * B = ", float(A) * int(B))
+A = float(input("please enter a percentage value (ex. .75) for A: "))
+B = int(input("please enter the value A will be multiplied by: "))
+print("A * B = ", A * B)
